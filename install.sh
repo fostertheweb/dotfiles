d50904ecc63859bf6d08d7577bafc258a3c394ac
Jonathan Foster <jfost784@gmail.com>
2017-01-06 18:51:25 -0500

    install ohmyzsh via script

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
