59b8c6f8335a4104d353a2e94bd549635f957102
Jonathan Foster <jxf9099@rit.edu>
2018-07-27 21:23:37 -0400

    some smarter stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,2 @@
 set -gx EDITOR nvim
 set -gx GDK_SCALE 2
-
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# install vim plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# packages
-# fzf diff-so-fancy hub the_silver_searcher
