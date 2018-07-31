190fd2ac7ff46f44808246a02cc75694d92bedcb
Jonathan Foster <jxf9099@rit.edu>
2018-07-31 13:14:23 -0400

    flatpak is not my friend

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
 set -gx EDITOR nvim
 set -gx TERM rxvt-unicode-256color
 set -gx XDG_DATA_HOME $HOME/.local/share
-set -gx XDG_DATA_DIRS /var/lib/flatpak/exports/share
+set -gx XDG_DATA_DIRS $XDG_DATA_DIRS:/var/lib/flatpak/exports/share
