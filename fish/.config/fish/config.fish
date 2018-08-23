1aa80e77f5faf9afd25f8b0ba97c64f3ea0792d5
Jonathan Foster <jxf9099@rit.edu>
2018-08-23 12:58:53 -0400

    gui fm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,6 @@ set -gx EDITOR vim
 set -gx TERM rxvt-unicode
 set -gx GDK_SCALE 2
 set -gx GDK_DPI_SCALE 0.5
-set -gx PATH $PATH $HOME/.local/bin $HOME/.node-modules/bin
+set -gx GOPATH $HOME/go
+set -gx PATH $PATH $HOME/.local/bin $HOME/.node-modules/bin $GOPATH/bin
 alias git=hub
-
