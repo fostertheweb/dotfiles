93e8ae55d703cad5a9f5ad8c1e1861f4596ec09b
Jonathan Foster <jxf9099@rit.edu>
2019-01-07 11:28:07 -0500

    Delete config.fish

--- fish/.config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-set -gx EDITOR vim
-set -gx TERM rxvt-unicode
-set -gx GDK_SCALE 2
-set -gx GDK_DPI_SCALE 0.5
-set -gx GOPATH $HOME/go
-set -gx PATH $PATH $HOME/.local/bin $HOME/.node-modules/bin $GOPATH/bin
-alias git=hub
