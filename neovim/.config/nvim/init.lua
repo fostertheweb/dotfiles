152540796cfcb2b2e22a347f6e7a876f934c3e4b
Jonathan Foster <jonathan.foster@zendesk.com>
2022-05-26 11:55:20 -0400

    lsp, treesitter, new colors

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -6,13 +6,52 @@ keymap('i', 'jj', '<Esc>', {})
 -- Plugins
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use 'ellisonleao/gruvbox.nvim'
+  use 'savq/melange'
+  use 'editorconfig/editorconfig-vim'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    config = function ()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          "bash",
+          "c",
+          "lua",
+          "rust",
+          "c_sharp",
+          "dockerfile",
+          "fish",
+          "go",
+          "javascript",
+          "typescript",
+          "tsx",
+        },
+        hightlight = {
+          enabled = true,
+        },
+        indent = {
+          enable = true,
+        }
+      })
+    end
+  }
+  use {
+    'williamboman/nvim-lsp-installer',
+    {
+      'neovim/nvim-lspconfig',
+      config = function ()
+      	require('nvim-lsp-installer').setup({
+          automatic_installation = true,
+      	})
+      end
+    }
+  }
 end)
 
--- General Config
+-- Options
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+vim.opt.termguicolors = true
 
-vim.cmd [[colorscheme gruvbox]]
+vim.cmd [[colorscheme melange]]
 
