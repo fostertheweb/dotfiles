f321ae864c31a22ab6c968e5d88efab105044b30
Jonathan Foster <fostertheweb@gmail.com>
2022-05-28 11:08:42 -0400

    telescope and start screen

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,12 +2,41 @@
 local keymap = vim.api.nvim_set_keymap
 
 keymap('i', 'jj', '<Esc>', {})
+keymap('n', '<C-p>', ':Telescope git_files<CR>', {})
 
 -- Plugins
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'savq/melange'
+  use {
+    'goolord/alpha-nvim',
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.startify'.config)
+    end
+  }
   use 'editorconfig/editorconfig-vim'
+  use 'kyazdani42/nvim-web-devicons'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function ()
+      require('lualine').setup {
+        options = {
+          theme = 'powerline'
+        }
+      }
+    end
+  }
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} },
+    config = function ()
+      local telescope = require('telescope')
+      telescope.setup()
+      telescope.load_extension('fzf')
+    end
+  }
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use {
     'lewis6991/gitsigns.nvim',
     config = function ()
