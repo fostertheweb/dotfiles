ebd7e42737b67cb9569fe726d43504e59a721e65
Jonathan Foster <fostertheweb@gmail.com>
2022-06-06 22:39:06 -0400

    format on save

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,7 +3,8 @@ local keymap = vim.api.nvim_set_keymap
 
 keymap("i", "jj", "<Esc>", {})
 keymap("n", "<C-p>", ":Telescope git_files<CR>", {})
-keymap("n", "<C-f>", ":lua vim.lsp.buf.formatting_sync()<CR>", {})
+keymap("n", "<C-f>", ":Telescope live_grep<CR>", {})
+keymap("n", "<C-t>", ":Telescope lsp_document_symbols<CR>", {})
 
 -- Plugins
 require("packer").startup(function(use)
@@ -91,7 +92,21 @@ require("packer").startup(function(use)
   use({
     "jose-elias-alvarez/null-ls.nvim",
     config = function()
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
       require("null-ls").setup({
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                vim.lsp.buf.formatting_sync()
+              end,
+            })
+          end
+        end,
         sources = {
           require("null-ls").builtins.formatting.stylua,
           require("null-ls").builtins.formatting.prettier,
