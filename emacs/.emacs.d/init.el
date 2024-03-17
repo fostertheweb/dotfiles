c9b0835909dff545ed0d9b9d4e1fb319585fd5b8
Jonathan Foster <fostertheweb@gmail.com>
2024-03-17 04:11:58 -0400

    git gutters

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -63,7 +63,7 @@
   :ensure t
   :config
   (setq vertico-cycle t)
-  (setq vertico-resize t)
+  (setq vertico-resize nil)
   (vertico-mode 1))
 
 (use-package consult
@@ -113,6 +113,17 @@
   :ensure t
   :config
   (ef-themes-select 'ef-symbiosis))
+
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
 
 (use-package lsp-mode
   :init
