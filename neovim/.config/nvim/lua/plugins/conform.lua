d613e04a7e898bec4810c456ce6aec08965e97de
Jonathan Foster <fostertheweb@gmail.com>
2024-11-18 00:39:15 -0500

    new conform config

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -2,15 +2,17 @@ return {
   'stevearc/conform.nvim',
   opts = {
     notify_on_error = false,
+    default_format_opts = {
+      lsp_format = 'fallback',
+    },
     format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
+      timeout_ms = 2000,
     },
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { 'gofmt', 'goimports' },
-      javascript = { { 'prettierd', 'prettier' } },
-      typescript = { { 'prettierd', 'prettier' } },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
 }
