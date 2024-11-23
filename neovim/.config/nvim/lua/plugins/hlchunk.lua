661fb9d761347192f7d75ffae1b808e2771507de
Jonathan Foster <jonathan.foster@prizepicks.com>
2024-11-23 02:42:53 -0500

    https://github.com/shellRaining/hlchunk.nvim/pull/152

--- neovim/.config/nvim/lua/plugins/hlchunk.lua
+++ neovim/.config/nvim/lua/plugins/hlchunk.lua
@@ -1,5 +1,6 @@
 return {
-  'shellRaining/hlchunk.nvim',
+  'ediezindell/hlchunk.nvim',
+  commit = "370514d5e6de606bf5e6fa6f29b228994a90f797",
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
     require('hlchunk').setup {
