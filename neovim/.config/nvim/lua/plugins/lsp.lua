110b5303c92ecd3d0a0afef6ea0783b34cf306a4
Jonathan Foster <jonathan.foster@prizepicks.com>
2024-12-16 18:20:47 -0500

    will eslint fix it?

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -83,6 +83,15 @@ return {
       },
     }
 
+    require('lspconfig').eslint.setup {
+      on_attach = function(_, bufnr)
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          buffer = bufnr,
+          command = 'EslintFixAll',
+        })
+      end,
+    }
+
     require('mason').setup()
 
     local ensure_installed = vim.tbl_keys(servers or {})
