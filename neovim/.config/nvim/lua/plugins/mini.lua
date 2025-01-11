9fb1400379086f64042b5ff04e5e7db5c92b7beb
Jonathan Foster <fostertheweb@gmail.com>
2025-01-11 13:36:53 -0500

    autopairs, tmux branch color

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
   config = function()
     require('mini.ai').setup { n_lines = 500 }
     require('mini.indentscope').setup()
-    require('mini.pairs').setup()
     require('mini.surround').setup()
   end,
 }
