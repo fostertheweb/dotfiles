163b6d25c8697722f270abf960e1e59fe4f0f876
Jonathan Foster <fostertheweb@gmail.com>
2025-01-26 00:18:38 -0500

    pancakes

--- neovim/.config/nvim/lua/plugins/tui.lua
+++ neovim/.config/nvim/lua/plugins/tui.lua
@@ -18,10 +18,21 @@ local setup_tig_log = function()
   }
 end
 
+local setup_oterm = function()
+  require('tui').setup {
+    name = 'OtermOpen',
+    command = 'oterm',
+    width_margin = 0,
+    height_margin = 0.5,
+    border = 'none',
+  }
+end
+
 return {
   'ezechukwu69/tui.nvim',
   config = function()
     setup_tig_status()
     setup_tig_log()
+    setup_oterm()
   end,
 }
