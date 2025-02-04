7433ea66a366f614e11c9186a6b6577c2443e79d
Jonathan Foster <fostertheweb@gmail.com>
2025-02-03 22:55:13 -0500

    grep hidden files

--- zsh/.config/zsh/functions/search.zsh
+++ zsh/.config/zsh/functions/search.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
 function grep-cwd() {
-  ag . | fzf -e -i | sed 's/^\([^:]*\):\([0-9]*\):.*/\+\2 \1/' | xargs $EDITOR
+  ag --hidden . | fzf -e -i | sed 's/^\([^:]*\):\([0-9]*\):.*/\+\2 \1/' | xargs $EDITOR
 }
