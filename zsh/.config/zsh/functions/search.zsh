3a18deb20f20df9df7aed7cea93c758a37b73d92
Jonathan Foster <fostertheweb@gmail.com>
2025-02-25 13:47:22 -0500

    really fix grep function

--- zsh/.config/zsh/functions/search.zsh
+++ zsh/.config/zsh/functions/search.zsh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh
 
 function grep-cwd() {
-  $EDITOR $(ag --hidden . | fzf -e -i | sed 's/^\([^:]*\):\([0-9]*\):.*/\+\2 \1/')
+  local selection=$(ag --hidden . | fzf -e -i)
+  if [[ -n "$selection" ]]; then
+    local file=$(echo "$selection" | sed -E 's/^([^:]*):([0-9]*):.*/\1/')
+    local line=$(echo "$selection" | sed -E 's/^([^:]*):([0-9]*):.*/\2/')
+    $EDITOR +$line "$file"
+  fi
 }
 
 function find-file() {
