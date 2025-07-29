128bd34d90993adaf2c5fbe492100c7fadfba4a6
Jonathan Foster <fostertheweb@gmail.com>
2025-07-29 12:43:51 -0400

    feat(nvim): add native opencode.nvim plugin over embedded tui

--- neovim/.config/nvim/lua/plugins/appearance.lua
+++ neovim/.config/nvim/lua/plugins/appearance.lua
@@ -2,7 +2,11 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     cond = not vim.g.vscode,
-    ft = { 'markdown' },
+    opts = {
+      anti_conceal = { enabled = false },
+      file_types = { 'markdown', 'opencode_output' },
+    },
+    ft = { 'markdown', 'opencode_output' },
   },
   {
     'folke/todo-comments.nvim',
