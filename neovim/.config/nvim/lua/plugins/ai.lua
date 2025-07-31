46f06d7be837ead60b2a5c8bd1ce4b18e2fe6c89
Jonathan Foster <fostertheweb@gmail.com>
2025-07-31 10:01:18 -0400

    feat(nvim): customize opencode.nvim keymaps

--- neovim/.config/nvim/lua/plugins/ai.lua
+++ neovim/.config/nvim/lua/plugins/ai.lua
@@ -3,7 +3,41 @@ return {
     'sudo-tee/opencode.nvim',
     cond = not vim.g.vscode,
     config = function()
-      require('opencode').setup {}
+      require('opencode').setup {
+        preferred_picker = 'snacks',
+        default_global_keymaps = false,
+        keymap = {
+          window = {
+            next_message = ']]',
+            prev_message = '[[',
+            prev_prompt_history = '<up>',
+            next_prompt_history = '<down>',
+            submit = '<C-j>',
+            submit_insert = '<C-j>',
+            switch_mode = '<C-s>',
+          },
+        },
+      }
+
+      vim.keymap.set('n', '<leader>oo', require('opencode.api').toggle, { desc = 'Toggle' })
+      vim.keymap.set('n', '<leader>os', require('opencode.api').select_session, { desc = 'Sessions' })
+      vim.keymap.set('n', '<leader>od', require('opencode.api').diff_open, { desc = 'Diff' })
+      -- revert all last promopt
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'opencode_input', 'opencode_output' },
+        callback = function()
+          vim.keymap.set('n', 'gd', require('opencode.api').diff_open, { desc = 'Diff' })
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'opencode_diff' },
+        callback = function()
+          -- ]c [c kemaps for next/prev diff
+          -- revert this files
+        end,
+      })
     end,
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -19,7 +53,7 @@ return {
     },
     config = function()
       require('supermaven-nvim').setup {
-        ignore_filetypes = { 'copilot-chat' },
+        ignore_filetypes = { 'opencode_input' },
         keymaps = {
           accept_suggestion = '<C-f>',
           clear_suggestion = '<C-k>',
