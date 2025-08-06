3684e4d1878fa6d897077d2ff0d7a1181c535f7d
Jonathan Foster <fostertheweb@gmail.com>
2025-08-05 23:44:55 -0400

    feat(nvim,ghostty): ashen

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -19,7 +19,7 @@ require('lazy').setup {
 
 -- Default theme settings
 vim.o.background = 'dark'
-vim.cmd 'colorscheme melange'
+vim.cmd 'colorscheme ashen'
 vim.opt.termguicolors = true
 vim.cmd.hi 'Comment gui=none'
 
