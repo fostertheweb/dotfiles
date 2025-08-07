a128b791dd689bd12a019d96eccda053e74038e0
Jonathan Foster <fostertheweb@gmail.com>
2025-08-07 16:38:14 -0400

    feat(nvim): add ts-error-translator

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,12 @@
 return {
-  'neovim/nvim-lspconfig',
-  cond = not vim.g.vscode,
+  {
+    'neovim/nvim-lspconfig',
+    cond = not vim.g.vscode,
+  },
+  {
+    'dmmulroy/ts-error-translator.nvim',
+    config = function()
+      require('ts-error-translator').setup()
+    end,
+  },
 }
