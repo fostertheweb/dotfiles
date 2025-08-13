858c544af8db0fe3993715ae922150ee46698908
Jonathan Foster <fostertheweb@gmail.com>
2025-08-13 00:23:49 -0400

    feat(nvim): testing flatten.nvim with git commit

--- neovim/.config/nvim/lua/plugins/system.lua
+++ neovim/.config/nvim/lua/plugins/system.lua
@@ -1,3 +1,14 @@
 return {
-  { 'brianhuster/unnest.nvim' },
+  {
+    'willothy/flatten.nvim',
+    enabled = true,
+    lazy = false,
+    priority = 1001,
+    opts = {
+      window = {
+        open = 'tab',
+      },
+    },
+  },
+  { 'brianhuster/unnest.nvim', enabled = false },
 }
