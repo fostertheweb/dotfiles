e7635fc443689d1bba96ddc488c7cea8bc83871a
Jonathan Foster <fostertheweb@gmail.com>
2025-08-12 23:55:51 -0400

    feat(nvim): add tailwind-tools

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,16 @@ return {
     'neovim/nvim-lspconfig',
   },
   {
+    'luckasRanarison/tailwind-tools.nvim',
+    name = 'tailwind-tools',
+    build = ':UpdateRemotePlugins',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'neovim/nvim-lspconfig',
+    },
+    opts = {},
+  },
+  {
     'dmmulroy/ts-error-translator.nvim',
     config = function()
       require('ts-error-translator').setup()
