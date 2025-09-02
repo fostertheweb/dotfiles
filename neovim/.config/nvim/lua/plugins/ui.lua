89587db7f93effaa7e5b5bb35ca950c45c0f1475
Jonathan Foster <fostertheweb@gmail.com>
2025-09-02 14:09:19 -0400

    feat(nvim): remove zen-mode

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -217,29 +217,4 @@ return {
       }
     end,
   },
-  {
-    'folke/zen-mode.nvim',
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      window = {
-        backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-        width = 120, -- width of the Zen window
-        height = 1, -- height of the Zen window
-        options = {
-          signcolumn = 'no', -- disable signcolumn
-          number = false, -- disable number column
-          relativenumber = false, -- disable relative numbers
-          cursorline = false, -- disable cursorline
-          -- cursorcolumn = false, -- disable cursor column
-          foldcolumn = '0', -- disable fold column
-          -- list = false, -- disable whitespace characters
-        },
-      },
-      plugins = {
-        options = { enabled = true }, -- disable/show UI bits
-      },
-    },
-  },
 }
