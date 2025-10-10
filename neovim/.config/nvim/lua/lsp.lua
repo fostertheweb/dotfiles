8b5055010a4d22fb80b39511d03d99f9fd497d80
Jonathan Foster <fostertheweb@gmail.com>
2025-10-10 15:33:32 -0400

    fix(nvim): stop noisy lsp error from navic

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -19,7 +19,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
     local client = vim.lsp.get_client_by_id(event.data.client_id)
 
-    require('nvim-navic').attach(client, event.buf)
+    if client:supports_method 'textDocument/documentSymbol' then
+      require('nvim-navic').attach(client, event.buf)
+    end
 
     if client:supports_method 'textDocument/completion' then
       vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
