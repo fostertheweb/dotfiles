7bc0079965911bf31293914ec076b8bbd4c53730
Jonathan Foster <fostertheweb@gmail.com>
2025-10-17 17:20:17 -0400

    refactor(nvim): remove statusline and navic navigation integration
    - Hide statusline completely by changing laststatus to 0
    - Remove navic highlights from init.lua
    - Remove navic LSP attachment for document symbols

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -23,8 +23,4 @@ vim.opt.termguicolors = true
 vim.cmd 'colorscheme kanagawa'
 vim.cmd.hi 'Comment gui=none'
 
-vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NONE' })
-vim.api.nvim_set_hl(0, 'NavicText', { bg = 'NONE', fg = vim.api.nvim_get_hl(0, { name = 'Comment' }).fg })
-vim.api.nvim_set_hl(0, 'NavicSeparator', { bg = 'NONE', fg = vim.api.nvim_get_hl(0, { name = 'Comment' }).fg })
-
 -- vim: ts=2 sts=2 sw=2 et
