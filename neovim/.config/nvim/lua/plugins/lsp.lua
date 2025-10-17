888f7b0e5425ad048305947e2a9e6d3a5c0afebd
Jonathan Foster <fostertheweb@gmail.com>
2025-10-17 01:33:59 -0400

    refactor(nvim): remove unused LSP plugins and disable snacks preview

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,18 +1,5 @@
 return {
   {
-    'neovim/nvim-lspconfig',
-  },
-  {
-    'luckasRanarison/tailwind-tools.nvim',
-    name = 'tailwind-tools',
-    build = ':UpdateRemotePlugins',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'neovim/nvim-lspconfig',
-    },
-    opts = {},
-  },
-  {
     'dmmulroy/ts-error-translator.nvim',
     config = function()
       require('ts-error-translator').setup()
