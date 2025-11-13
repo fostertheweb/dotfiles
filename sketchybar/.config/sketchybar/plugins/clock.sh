24363afa02ab51f3db5c4c53f7328b0fc1fc36e1
Jonathan Foster <fostertheweb@gmail.com>
2025-11-13 15:47:28 -0500

    feat(sketchybar): clock update

--- sketchybar/.config/sketchybar/plugins/clock.sh
+++ sketchybar/.config/sketchybar/plugins/clock.sh
@@ -3,6 +3,11 @@
 source "$CONFIG_DIR/colors.sh"
 
 sketchybar --set "$NAME" \
+  background.drawing=on \
+  background.color="0x55000000" \
+  background.corner_radius=8 \
+  background.height=25 \
   label="$(date '+%H:%M')" \
   label.color="$WHITE" \
+  label.padding_left=10 \
   icon.drawing=off
