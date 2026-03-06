b96545259dd19b40bf2228e85ca455bb77328019
Jonathan Foster <fostertheweb@gmail.com>
2026-03-05 23:50:20 -0500

    refactor(sketchybar): remove configuration

--- sketchybar/.config/sketchybar/plugins/front_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ "$SENDER" = "front_app_switched" ]; then
-  BUNDLENAME=$(osascript -e "id of app \"$INFO\"")
-  sketchybar --set $NAME background.drawing=on background.image.string="app.$BUNDLENAME"
-fi
