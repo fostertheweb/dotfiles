d3a1f8cdd74f4c40f3987f43ddd2582afb7f6eac
Jonathan Foster <fostertheweb@gmail.com>
2026-03-15 01:06:10 -0400

    feat(nvim): add 99 ai plugin

--- neovim/.config/nvim/lua/plugins/ai.lua
+++ neovim/.config/nvim/lua/plugins/ai.lua
@@ -1,5 +1,39 @@
 return {
   {
+    'ThePrimeagen/99',
+    config = function()
+      local _99 = require '99'
+
+      _99.setup {
+        tmp_dir = './tmp',
+        completion = {
+          files = {
+            enabled = true,
+            max_file_size = 102400,
+            max_files = 5000,
+            exclude = { '.env', '.env.*', 'node_modules', '.git' },
+          },
+          source = 'native',
+        },
+        md_files = {
+          'AGENT.md',
+        },
+      }
+
+      vim.keymap.set('v', '<leader>9v', function()
+        _99.visual()
+      end)
+
+      vim.keymap.set('n', '<leader>9x', function()
+        _99.stop_all_requests()
+      end)
+
+      vim.keymap.set('n', '<leader>9s', function()
+        _99.search()
+      end)
+    end,
+  },
+  {
     'zbirenbaum/copilot.lua',
     enabled = true,
     dependencies = { 'AndreM222/copilot-lualine' },
