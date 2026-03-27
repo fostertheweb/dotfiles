8783c9a1ce3742ea1e142bff1bf6564a519e10aa
Jonathan Foster <fostertheweb@gmail.com>
2026-03-27 14:36:06 -0400

    feat(nvim): mini.completion

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -16,6 +16,14 @@ return {
       },
     }
 
+    require('mini.completion').setup {}
+
+    local imap_expr = function(lhs, rhs)
+      vim.keymap.set('i', lhs, rhs, { expr = true })
+    end
+    imap_expr('<Tab>', [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+    imap_expr('<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+
     local miniclue = require 'mini.clue'
     miniclue.setup {
       triggers = {
