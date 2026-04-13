18c7d5ffb8a036dfd9f5ae5c219402133654673e
Jonathan Foster <fostertheweb@gmail.com>
2026-04-13 15:30:33 -0400

    feat(nvim): add koda theme, remove eyes and jellybeans themes

--- neovim/.config/nvim/plugin/theme.lua
+++ neovim/.config/nvim/plugin/theme.lua
@@ -3,11 +3,10 @@ local utils = require 'utils'
 vim.pack.add {
   'https://github.com/zitrocode/carvion.nvim',
   'https://github.com/lucasadelino/conifer.nvim',
-  'https://github.com/amedoeyes/eyes.nvim',
-  'https://github.com/wtfox/jellybeans.nvim',
   'https://github.com/savq/melange-nvim',
   'https://github.com/thallada/farout.nvim',
   'https://github.com/shoenot/witchesbrew.nvim',
+  'http://github.com/oskarnurm/koda.nvim',
 }
 
 require('conifer').setup {
@@ -21,11 +20,6 @@ if utils.is_work_computer() then
   vim.o.background = 'dark'
   vim.cmd 'colorscheme melange'
 else
-  if utils.is_dark_mode() then
-    vim.o.background = 'dark'
-    vim.cmd 'colorscheme conifer'
-  else
-    vim.o.background = 'light'
-    vim.cmd 'colorscheme jellybeans'
-  end
+  vim.o.background = 'dark'
+  vim.cmd 'colorscheme koda'
 end
