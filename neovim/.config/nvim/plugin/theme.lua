dedc619430ea29f45a03fff47b76644b2f025664
Jonathan Foster <fostertheweb@gmail.com>
2026-05-05 18:19:06 -0400

    feat(nvim): switch to witchesbrew-bright and clean up old themes

--- neovim/.config/nvim/plugin/theme.lua
+++ neovim/.config/nvim/plugin/theme.lua
@@ -2,31 +2,22 @@ local utils = require 'utils'
 
 vim.pack.add {
   'https://github.com/zitrocode/carvion.nvim',
-  'https://github.com/lucasadelino/conifer.nvim',
   'https://github.com/thallada/farout.nvim',
-  'http://github.com/oskarnurm/koda.nvim',
   'https://github.com/savq/melange-nvim',
-  'https://github.com/pisgahk/muted.nvim',
   'https://github.com/reobin/olive-crt.nvim',
   'https://github.com/shoenot/witchesbrew.nvim',
 }
 
-require('conifer').setup {
-  transparent = false,
-}
-
 -- Default theme settings
 vim.cmd.hi 'Comment gui=none'
+vim.o.background = 'dark'
 
 if utils.is_work_computer() then
-  vim.o.background = 'dark'
-  vim.cmd 'colorscheme muted_autumn'
+  vim.cmd 'colorscheme witchesbrew-bright'
 else
   if utils.is_dark_mode() then
-    vim.o.background = 'dark'
-    vim.cmd 'colorscheme koda'
+    vim.cmd 'colorscheme carvion'
   else
-    vim.o.background = 'light'
-    vim.cmd 'colorscheme koda-moss'
+    vim.cmd 'colorscheme olive-crt'
   end
 end
