9f9e50f3dfd6d37bda5c5e4436c6b05225c9a7cb
Jonathan Foster <fostertheweb@gmail.com>
2026-07-09 01:20:48 -0400

    feat(emacs): go emacs

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -8,8 +8,14 @@
    '("998bc02f2e52205ad06df88a14d53168aef1ec1bbcc6fe2b0cd15fed8e5c8dae"
      default))
  '(package-selected-packages
-   '(better-jumper consult-fd corfu dired-subtree ef-themes ghostel magit
-		   marginalia modus-themes nerd-icons
-		   nerd-icons-completion nerd-icons-corfu
+   '(better-jumper consult-fd corfu dired-subtree ef-themes flycheck
+		   ghostel magit marginalia meow modus-themes
+		   nerd-icons nerd-icons-completion nerd-icons-corfu
 		   nerd-icons-dired orderless spacious-padding trashed
 		   vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
