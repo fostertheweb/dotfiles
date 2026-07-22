143867c578b084f13d834262405e263871ff06b5
Jonathan Foster <jonathan.foster@prizepicks.com>
2026-07-22 14:01:15 -0400

    feat(ghostty): theme, font

--- neovim/.config/nvim/plugin/theme.lua
+++ neovim/.config/nvim/plugin/theme.lua
@@ -5,7 +5,19 @@ vim.pack.add {
   'https://github.com/thallada/farout.nvim',
   'https://github.com/savq/melange-nvim',
   'https://github.com/reobin/olive-crt.nvim',
+  'https://github.com/arnauKL/south.nvim',
   'https://github.com/shoenot/witchesbrew.nvim',
+}
+
+require('south').setup {
+  transparent = false,
+  darker_floats = true,
+  styles = {
+    italics = true,
+    italic_comments = true,
+    italic_linenums = false,
+    bold_keywords = true,
+  },
 }
 
 -- Default theme settings
@@ -13,7 +25,11 @@ vim.cmd.hi 'Comment gui=none'
 vim.o.background = 'dark'
 
 if utils.is_work_computer() then
-  vim.cmd 'colorscheme witchesbrew-bright'
+  if utils.is_dark_mode() then
+    vim.cmd 'colorscheme carvion'
+  else
+    vim.cmd 'colorscheme south'
+  end
 else
   if utils.is_dark_mode() then
     vim.cmd 'colorscheme carvion'
--- zsh/.config/zsh/config.zsh
+++ zsh/.config/zsh/config.zsh
@@ -1,17 +1,15 @@
 #!/usr/bin/env zsh
 
 export ENHANCE_THEME=medallion
+export TERMINAL_FONT_FAMILY="IosevkaTerm Nerd Font Mono"
+export TERMINAL_FONT_SIZE=15
 
 if [[ "$(whoami)" == "jonathan.foster" ]]; then
 	# work computer
-	export TERMINAL_FONT_FAMILY="BlexMono Nerd Font Mono"
-	export TERMINAL_FONT_SIZE=14
-	export GHOSTTY_THEME="witchesbrew-bright"
+	export GHOSTTY_THEME="light:south,dark:carvion"
 	export DEFAULT_AGENT="codex"
 else
 	# personal computer
-	export TERMINAL_FONT_FAMILY="BerkeleyMono Nerd Font"
-	export TERMINAL_FONT_SIZE=14
 	export GHOSTTY_THEME="light:olive-crt,dark:carvion"
 	export DEFAULT_AGENT="pi"
 fi
